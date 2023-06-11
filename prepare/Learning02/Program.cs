e44070446b767e7a21bf3aaddcc4fd60ce67c589
Carol Ann Maxwell <C.maxwell021@gmail.com>
2023-06-11 20:40:23 +0200

    lesson02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Operations Manager";
+        job1._company = "Avatar Health";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "General Manager";
+        job2._company = "Packratb cc";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Carol Maxwell";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
